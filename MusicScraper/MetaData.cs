c9829214c1b2a82af19ddc2d0d55d2d4ee8da9a8
Virusnest <kai.maloney.au@gmail.com>
2024-07-16 14:47:32 +1000

    Inital Cmmit

---
+++ MusicScraper/MetaData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace MusicScraper
+{
+	public class MetaData
+	{
+		public string Title, Artist, Album;
+		public int Year;
+
+		public MetaData(string title="N/A", string artist = "N/A", string album = "N/A", int year = 0)
+		{
+			Title = title;
+			Artist = artist;
+			Album = album;
+			Year = year;
+		}
+		public override string ToString()
+		{
+			return $"Title: {Title}, Artist: {Artist}, Album: {Album}, Year: {Year}, ";
+		}
+	}
+}
+
---
+++ MusicScraper/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MusicScraper;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using System;
+class Program
+{
+	public static SpotifyClient? client;
+	static void Main(string[] args)
+	{
+
+		Connect();
+		Console.WriteLine(Search("MANESKIN-Beggin").Result.ToString());
+		Console.Read();
+		
+	}
+	static void Connect()
+	{
+
+		var config = SpotifyClientConfig.CreateDefault();
+
+		var request = new ClientCredentialsRequest("803aa0f3be4c486f8978e42452c0d2a0", "b9fba9ee5cd042549ee801b3603b7f30");
+		var response = new OAuthClient(config).RequestToken(request);
+
+		client = new SpotifyClient(config.WithToken(response.Result.AccessToken));
+		
+	
+	}
+	public async static Task<MetaData> Search(string wawa)
+	{
+		var data = new MetaData();
+		if (client == null) { return data; }
+		SearchRequest trackSearch = new SearchRequest(SearchRequest.Types.Track,wawa);
+		SearchRequest artistSearch = new SearchRequest(SearchRequest.Types.Track, wawa);
+		SearchRequest albumSearch = new SearchRequest(SearchRequest.Types.Track, wawa);
+		var trackResponce = await client.Search.Item(trackSearch);
+		if (trackResponce.Tracks.Items != null) {
+			data.Title = trackResponce.Tracks.Items[0].Name;
+			data.Artist = trackResponce.Tracks.Items[0].Artists[0].Name;
+			data.Album = trackResponce.Tracks.Items[0].Album.Name;
+			data.Year = Convert.ToInt32(trackResponce.Tracks.Items[0].Album.ReleaseDate.Remove(4));
+		}
+
+
+		return data;
+	}
+
+}
